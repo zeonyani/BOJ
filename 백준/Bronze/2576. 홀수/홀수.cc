90d13246fcfbaac7184f4c213d20464e7334e2ef
zeonyani <52484590+zeonyani@users.noreply.github.com>
2025-08-07 14:22:04 +0900

    [Bronze III] Title: 홀수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2576. 홀수/홀수.cc
+++ 백준/Bronze/2576. 홀수/홀수.cc
@@ -1,27 +1,21 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
+
 int main(void)
 {
-	cin.tie(0); ios_base::sync_with_stdio(0); // 코테 속도 up
-
-	int tmp; // 숫자 받을 임시 변수
-	vector<int> odds; // 홀수만 저장할 벡터 (사이즈 미지정)
-	int sum = 0;
-	for (int i = 0; i < 7; i++) { // 7회 숫자 받음
-		cin >> tmp;
-		if (tmp % 2 == 1) odds.push_back(tmp);
-	}
-	sort(odds.begin(), odds.end());
-	if (odds.size() == 0) cout << "-1";
-	else {
-		for (int i = 0; i < odds.size(); i++) {
-			sum += odds[i];
-		}
-		cout << sum << "\n";
-		cout << odds[0];
-	}
+	cin.tie(0); ios_base::sync_with_stdio(0); // 코테 속도 빠르게
+	int tmp; // 숫자 받는 임시 변수
+	int min = 0x3f3f3f3f; // 최소값 저장용(엄청 큰 값으로)
+	int sum = 0; // 홀수 합
 	
+	for (int i = 0; i < 7; i++) {
+		cin >> tmp;
+		if (tmp & 1) {
+			sum += tmp;
+			if (tmp < min) min = tmp;
+		}
+	}
+	if (sum == 0) cout << "-1";
+	else cout << sum << "\n" << min;
 	return 0;
 }
