34f546d5239603d64734e0d19f8e7f2eb7edfe5b
zeonyani <52484590+zeonyani@users.noreply.github.com>
2025-08-27 10:24:21 +0900

    [Bronze II] Title: 숫자, Time: 12 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10093. 숫자/숫자.cc
+++ 백준/Bronze/10093. 숫자/숫자.cc
@@ -1,32 +1,23 @@
 #include <iostream>
+#include <algorithm> // swap 사용을 위함
 using namespace std;
 int main(void)
 {
 	cin.tie(0); ios_base::sync_with_stdio(false);
 
-	int n1, n2;
+	long long n1, n2; // 조건2 충족을 위함
 	cin >> n1 >> n2;
 
-	// 값이 같은 경우 0 만 출력
 	if (n1 == n2) {
-		cout << 0;
-	}
-	// 후자가 더 작으면 교체
-	else  if (n1 > n2) {
-		int tmp;
-		tmp = n1;
-		n1 = n2;
-		n2 = tmp;
-
-		cout << n2 - n1 - 1 << "\n";
-		for (int i = n2 - n1 - 1; i > 0; i--) {
-			cout << n2 - i << " ";
-		}
+		cout << "0" << "\n";
 	}
 	else {
-		cout << n2 - n1 - 1 << "\n";
-		for (int i = n2 - n1 - 1; i > 0; i--) {
-			cout << n2 - i << " ";
+		// 전자가 더 크면 교체
+		if (n1 > n2) {
+			swap(n1, n2);
 		}
+		cout << n2 - n1 - 1 << "\n"; // 사이 정수 개수 출력
+		for (long long i = n1 + 1; i < n2; i++) cout << i << " "; // 그 정수 자체 출력
 	}
+	return 0;
 }
