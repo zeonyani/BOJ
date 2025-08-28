3a2de5464e4152509cfca81648c8830cde2ae522
zeonyani <52484590+zeonyani@users.noreply.github.com>
2025-08-28 15:27:50 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 192 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+	cin.tie(NULL); ios_base::sync_with_stdio(false);
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int a, b;
+		cin >> a >> b;
+		cout << a + b << "\n";
+	}
+	return 0;
+}
